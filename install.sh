45c3e03b941707d569cb95eb2df6d4865e6740ce
Issa Rice <riceissa@gmail.com>
2015-08-26 04:30:26 -0700

    add more symlinks etc

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 set -e
 
-#link to dotfiles using ln -s "$(pwd)/file" ~/.vimrc -- this is just an example
-
+# Vim and Neovim
+# --------------
 git clone git://github.com/riceissa/dotfiles.git
+mkdir -p ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #git clone git@github.com:riceissa/dotfiles.git
 cd dotfiles
 ln -s "$(pwd)/.vimrc" ~/.vimrc
 ln -s "$(pwd)/.gvimrc" ~/.gvimrc
-
- cd ~/.vim && git submodule update --init --recursive
+ln -s ~/.vimrc ~/.nvimrc
+ln -s ~/.vim ~/.nvim
+ln -s "$(pwd)/plugins.vim" ~/.vim/plugins.vim
+ln -s "$(pwd)/mswin_extract.vim" ~/.vim/mswin_extract.vim
+ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
+# For tmux
+echo 'source ~/.bashrc' >> ~/.bash_profile
+mkdir -p ~/.moc/themes
+ln -s "$(pwd)/.moc/config" ~/.moc/config
+ln -s "$(pwd)/.moc/themes/my_theme" ~/.moc/themes/my_theme
