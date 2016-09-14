7c736884915798aba741d4ca1db1d40d1b239fb5
Issa Rice <riceissa@gmail.com>
2016-09-14 14:42:43 -0700

    remove python2

--- vim-configure.sh
+++ vim-configure.sh
@@ -6,8 +6,6 @@
 ./configure --with-features=huge \
   --enable-multibyte \
   --enable-rubyinterp \
-  --enable-pythoninterp \
-  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
   --enable-python3interp \
   --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
   --enable-perlinterp \
