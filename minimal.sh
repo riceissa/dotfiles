2e47d1acac014a00bac11f1c4f1a39ce8aa56525
Issa Rice <riceissa@gmail.com>
2016-11-05 01:13:41 -0700

    quote EOF

--- minimal.sh
+++ minimal.sh
@@ -23,7 +23,7 @@ EOF
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cat <<EOF >> ~/.vimrc
+cat <<'EOF' >> ~/.vimrc
 set nocompatible
 call plug#begin('~/.vim/plugged')
 Plug 'riceissa/vim-emacsctrll'
