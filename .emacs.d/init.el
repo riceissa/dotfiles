b523ec73ae7917339b102bda90b048a215caa9d2
Issa Rice <riceissa@gmail.com>
2017-05-30 00:39:13 -0700

    use bar again

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cursor-type (quote bar))
  '(indent-tabs-mode nil)
  '(magit-diff-refine-hunk (quote all))
  '(make-backup-files nil)
