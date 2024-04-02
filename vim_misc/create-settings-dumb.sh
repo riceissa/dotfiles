35e74eb416539278b7f667cb2a17302fca57f3ee
Issa Rice <riceissa@gmail.com>
2024-04-02 16:34:11 -0700

    force write

--- vim_misc/create-settings-dumb.sh
+++ vim_misc/create-settings-dumb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-nvim -c 'source dump_settings.vim|write out-nvim.txt|quit'
+nvim -c 'source dump_settings.vim|write! out-nvim.txt|quit'
 
-/usr/bin/vim -c 'source dump_settings.vim|write out-vim.txt|quit'
+/usr/bin/vim -c 'source dump_settings.vim|write! out-vim.txt|quit'
