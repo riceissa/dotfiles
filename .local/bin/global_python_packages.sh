399ba46603a6795a734b8da34a8d45917514bc8d
Issa Rice <riceissa@gmail.com>
2024-10-01 13:26:16 -0700

    force install for python packages that are unavailable on ubuntu

--- .local/bin/global_python_packages.sh
+++ .local/bin/global_python_packages.sh
@@ -4,7 +4,7 @@
 # python packages here so that it's easy to get back what will be missing.
 
 sudo apt install python3-typer yt-dlp python3-mysql.connector urlwatch python3-html2text python3-bs4 python3-mypy python3-dateutil python3-oauth2client python3-googleapi python3-minidb python3-requests python3-keyring python3-appdirs python3-lxml python3-cssselect
-pip3 install --upgrade pyyaml
-pip3 install --upgrade awscli
-pip3 install --upgrade google-analytics-data
-pip3 install --upgrade anki
+pip3 install --break-system-packages --upgrade pyyaml
+pip3 install --break-system-packages --upgrade awscli
+pip3 install --break-system-packages --upgrade google-analytics-data
+pip3 install --break-system-packages --upgrade anki
