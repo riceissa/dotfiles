ea322e4024493d4d625115db55d8c30b8a7ff3db
Issa Rice <riceissa@gmail.com>
2024-12-09 15:25:39 -0800

    updates

--- vim_misc/create-settings-dumb.sh
+++ vim_misc/create-settings-dumb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-nvim -c 'source dump_settings.vim|write! out-nvim.txt|quit'
+# nvim -Nu NONE -c 'source dump_settings.vim|write! out-nvim.txt|quit'
+# /usr/bin/vim -Nu ~/sensible.vim -c 'source dump_settings.vim|write! out-vim.txt|quit'
 
+nvim -c 'source dump_settings.vim|write! out-nvim.txt|quit'
 /usr/bin/vim -c 'source dump_settings.vim|write! out-vim.txt|quit'
