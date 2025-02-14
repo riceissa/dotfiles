9dbaec7c4cd0b95ea7af94a3eefb9b43df2b12d4
Issa Rice <riceissa@gmail.com>
2025-02-13 19:33:54 -0800

    add python3-typeshed

--- .local/bin/global_python_packages.sh
+++ .local/bin/global_python_packages.sh
@@ -3,7 +3,23 @@
 # Ubuntu deletes pip packages after a release upgrade, so keep all global
 # python packages here so that it's easy to get back what will be missing.
 
-sudo apt install python3-typer yt-dlp urlwatch python3-html2text python3-bs4 python3-mypy python3-dateutil python3-oauth2client python3-googleapi python3-yaml python3-minidb python3-requests python3-keyring python3-appdirs python3-lxml python3-cssselect
+sudo apt install python3-typer \
+    yt-dlp \
+    urlwatch \
+    python3-html2text \
+    python3-bs4 \
+    python3-mypy \
+    python3-dateutil \
+    python3-oauth2client \
+    python3-googleapi \
+    python3-yaml \
+    python3-minidb \
+    python3-requests \
+    python3-keyring \
+    python3-appdirs \
+    python3-lxml \
+    python3-cssselect \
+    python3-typeshed
 
 # Ubuntu 24.04 ships with Python 3.12 and mysql connector 8.0.15, but that
 # version of mysql connector uses a deprecated feature of python that no longer
