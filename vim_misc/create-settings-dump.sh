e82724614caeb0647de96a9e8f0b37c1c0fb344c
Issa Rice <riceissa@gmail.com>
2025-03-15 22:13:54 -0700

    this stuff has been moved to vim-uniform

--- vim_misc/create-settings-dump.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# nvim -Nu NONE -c 'source dump_settings.vim|write! out-nvim.txt|quit'
-# /usr/bin/vim -Nu ~/sensible.vim -c 'source dump_settings.vim|write! out-vim.txt|quit'
-
-nvim -c 'source dump_settings.vim|write! out-nvim.txt|quit'
-/usr/bin/vim -c 'source dump_settings.vim|write! out-vim.txt|quit'
