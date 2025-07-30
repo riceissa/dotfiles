637dc712c953f254910f03f54bd2234828915944
Issa Rice <riceissa@gmail.com>
2025-07-30 16:07:06 -0700

    finish cleaning up install script

--- install-linux.sh
+++ install-linux.sh
@@ -24,8 +24,6 @@ while [ -n "$1" ]; do
         ;;
     vim) install_vim=yes
         ;;
-    urxvt) install_urxvt=yes
-        ;;
     -h|--help) show_help=yes
         ;;
     esac
@@ -39,8 +37,8 @@ Install script for dotfiles.
 ./install [program...]
 ./install {-h|--help}
 
-Supported programs: bash, editorconfig,
-emacs, git, local_bin, moc, neovim, newsboat, proselint, tmux, vim, urxvt
+Supported programs: bash, editorconfig, emacs, git, kitty, local_bin, neovim,
+                    newsboat, proselint, tmux, vim
 
 For instance to install dotfiles for Vim and tmux, run:
 
@@ -48,10 +46,6 @@ For instance to install dotfiles for Vim and tmux, run:
 EOF
     exit
 fi
-
-readlink_or_filepath() {
-    readlink "$1" || echo "$1"
-}
 
 if [ -n "$install_bash" ]; then
     if grep -q -e "^HISTSIZE" ~/.bashrc; then
@@ -75,13 +69,32 @@ if [ -n "$install_bash" ]; then
         echo "HISTCONTROL not found; adding HISTCONTROL=ignoreboth:erasedups"
         echo "HISTCONTROL=ignoreboth:erasedups" >> ~/.bashrc
     fi
-    if grep -q -F "dotfiles/bash/common.bash" ~/.bashrc; then
+    common_bash_line="[ -f $(pwd)/bash/common.bash ] && source $(pwd)/bash/common.bash"
+    if grep -q -F "$common_bash_line" ~/.bashrc; then
         echo "common.bash found in bashrc; not doing anything"
     else
         echo "common.bash not found in bashrc; adding line to source it"
-        common_bash_location="$(pwd)/bash/common.bash"
-        echo "[ -f $common_bash_location ] && source $common_bash_location" >> ~/.bashrc
+        echo "$common_bash_line" >> ~/.bashrc
     fi
+fi
+
+if [ -n "$install_editorconfig" ]; then
+    ln -sv "$(pwd)/.editorconfig" ~/.editorconfig
+fi
+
+if [ -n "$install_emacs" ]; then
+    mkdir -p ~/.emacs.d
+    ln -sv "$(pwd)/.emacs.d/init.el" ~/.emacs.d/init.el
+fi
+
+if [ -n "$install_git" ]; then
+    ln -sv "$(pwd)/.gitconfig" ~/.gitconfig
+    ln -sv "$(pwd)/.cvsignore" ~/.cvsignore
+fi
+
+if [ -n "$install_kitty" ]; then
+    mkdir -p ~/.config/kitty
+    ln -sv "$(pwd)/.config/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 fi
 
 if [ -n "$install_local_bin" ]; then
@@ -91,56 +104,10 @@ if [ -n "$install_local_bin" ]; then
     fi
 fi
 
-if [ -n "$install_vim" ]; then
-    # Get vim-plug to manage plugins
-    if command -v curl >/dev/null 2>&1; then
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    elif command -v wget >/dev/null 2>&1; then
-        mkdir -p ~/.vim/autoload
-        wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
-            -O ~/.vim/autoload/plug.vim
-    else
-        echo "Could not fetch vim-plug; neither curl nor wget exists."
-    fi
-
-    mv -v ~/.vimrc ~/.vimrc.$(date -Idate).bak 2> /dev/null
-    ln -svf "$(pwd)/.vimrc" ~/.vimrc
-fi
-
 if [ -n "$install_neovim" ]; then
     config_home=${XDG_CONFIG_HOME:=$HOME/.config}
     mkdir -p $config_home/nvim
     ln -sv "$(pwd)/.config/nvim/init.vim" $config_home/nvim/init.vim
-fi
-
-if [ -n "$install_tmux" ]; then
-    ln -svf "$(pwd)/.tmux.conf" ~/.tmux.conf
-    # tmux doesn't read from .bashrc so copy contents to .bash_profile
-    # Actually I don't think this is necessary?
-    # echo 'source ~/.bashrc' >> ~/.bash_profile
-fi
-
-if [ -n "$install_editorconfig" ]; then
-    ln -sv "$(pwd)/.editorconfig" ~/.editorconfig
-fi
-
-if [ -n "$install_urxvt" ]; then
-    mv -v ~/.Xresources ~/.Xresources.$(date -Idate).bak 2> /dev/null
-    ln -svf "$(pwd)/.Xresources" ~/.Xresources
-
-    mkdir -p ~/.urxvt/ext
-    ln -svf "$(pwd)/.urxvt/ext/clipboard" ~/.urxvt/ext/clipboard
-fi
-
-if [ -n "$install_kitty" ]; then
-    mkdir -p ~/.config/kitty
-    ln -sv "$(pwd)/.config/kitty/kitty.conf" ~/.config/kitty/kitty.conf
-fi
-
-if [ -n "$install_git" ]; then
-    ln -sv "$(pwd)/.gitconfig" ~/.gitconfig
-    ln -sv "$(pwd)/.cvsignore" ~/.cvsignore
 fi
 
 if [ -n "$install_newsboat" ]; then
@@ -148,13 +115,16 @@ if [ -n "$install_newsboat" ]; then
     ln -sv "$(pwd)/.newsboat/config" ~/.newsboat/config
 fi
 
-if [ -n "$install_emacs" ]; then
-    mkdir -p ~/.emacs.d
-    ln -sv "$(pwd)/.emacs.d/init.el" ~/.emacs.d/init.el
-fi
-
 if [ -n "$install_proselint" ]; then
     config_home=${XDG_CONFIG_HOME:=$HOME/.config}
     mkdir -p "$config_home/proselint"
-    ln -svf "$(pwd)/.config/proselint/config.json" "$config_home/proselint/config.json"
+    ln -sv "$(pwd)/.config/proselint/config.json" "$config_home/proselint/config.json"
+fi
+
+if [ -n "$install_tmux" ]; then
+    ln -sv "$(pwd)/.tmux.conf" ~/.tmux.conf
+fi
+
+if [ -n "$install_vim" ]; then
+    ln -sv "$(pwd)/.vimrc" ~/.vimrc
 fi
