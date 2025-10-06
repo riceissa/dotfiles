f4f200c4b68a5d2bd9fcb0891e39d39506ba76b2
Issa Rice <riceissa@gmail.com>
2025-10-05 19:05:16 -0700

    fix ctrl+l in other terminals

--- bash/kitty.bash
+++ bash/kitty.bash
@@ -1,11 +1,13 @@
-# Make ctrl+l in kitty work the way it does in other terminals
-# See https://sw.kovidgoyal.net/kitty/conf/#shortcut-kitty.Reset-the-terminal
-# for more information.
-clear-screen-saving-contents-in-scrollback() {
-    # The kitty documentation lists both of these without really explaining how
-    # they are different, and when I test it they both seem to do the same
-    # thing...
-    # printf "\e[H\e[22J"
-    printf "\r\e[0J\e[H\e[22J"
-}
-bind -x '"\C-l": clear-screen-saving-contents-in-scrollback'
+if [ $TERM == "xterm-kitty" ]; then
+    # Make ctrl+l in kitty work the way it does in other terminals
+    # See https://sw.kovidgoyal.net/kitty/conf/#shortcut-kitty.Reset-the-terminal
+    # for more information.
+    clear-screen-saving-contents-in-scrollback() {
+        # The kitty documentation lists both of these without really explaining how
+        # they are different, and when I test it they both seem to do the same
+        # thing...
+        # printf "\e[H\e[22J"
+        printf "\r\e[0J\e[H\e[22J"
+    }
+    bind -x '"\C-l": clear-screen-saving-contents-in-scrollback'
+fi
