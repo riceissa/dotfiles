e2be4373a4f21350bf8600e060bfec79de1f6648
Issa Rice <riceissa@gmail.com>
2026-09-11 11:04:24 -0700

    remove

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,8 +47,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-code-face ((t nil)))
- '(markdown-inline-code-face ((t nil))))
+)
 
 (setq mymediawiki-highlights
       '(("<ref[^>/]*>?[^<]*\\(</ref>\\|/>\\)" . font-lock-constant-face)))
